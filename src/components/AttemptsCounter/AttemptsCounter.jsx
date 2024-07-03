b9ab11f099f4a8a15d80d91c8ffef6162052d8f3
Aleksei Erofeev <eao173a@gmail.com>
2024-07-03 23:34:28 +0800

    Updates

--- src/components/AttemptsCounter/AttemptsCounter.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./AttemptsCounter.module.css";
-
-export function AttemptsCounter({ value }) {
-  return (
-    <div className={styles.attemptsBlock}>
-      <span className={styles.attemptsText}>Осталось {value} попытки</span>
-    </div>
-  );
-}
--- src/hooks/useCustomContext.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { Context } from "../context/Context";
-
-export const useCustomContext = () => {
-  return useContext(Context);
-};
